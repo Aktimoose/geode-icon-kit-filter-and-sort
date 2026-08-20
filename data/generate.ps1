648e01b0c877ffa8201484217b0b14c3abf071c3
unknown <20.yasir.ahmed.02@gmail.com>
2026-08-20 16:59:09 -0400

    revert generate.ps1 changes

--- data/generate.ps1
+++ data/generate.ps1
@@ -1,18 +1,18 @@
 cat category-data |
-  python .\data-to-json.py |
-  python .\check-element-separation.py |
-  python .\check-valid-buckets-for-categories-and-sort.py |
-  python .\check-item-completeness.py |
-  python .\category-json-to-cpp.py |
+  python3 .\data-to-json.py |
+  python3 .\check-element-separation.py |
+  python3 .\check-valid-buckets-for-categories-and-sort.py |
+  python3 .\check-item-completeness.py |
+  python3 .\category-json-to-cpp.py |
   out-file -encoding utf8 ../src/constants/categories.cpp
 
 dos2unix ../src/constants/categories.cpp
 
 cat author-json.json |
-  python .\check-element-separation.py |
-  python .\check-valid-buckets-for-authors-and-sort.py |
-  python .\check-item-completeness.py |
-  python .\author-json-to-cpp.py |
+  python3 .\check-element-separation.py |
+  python3 .\check-valid-buckets-for-authors-and-sort.py |
+  python3 .\check-item-completeness.py |
+  python3 .\author-json-to-cpp.py |
   out-file -encoding utf8 ../src/constants/authors.cpp
 
 dos2unix ../src/constants/authors.cpp
